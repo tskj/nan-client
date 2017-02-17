3947a3f9d0fc792aee75082c5c5c89903397bbc3
tarjei <tarjei@skole.pc>
2017-02-17 19:24:01 +0100

    Implemented pane-switching

--- addressbook.js
+++ addressbook.js
@@ -1,1 +1,41 @@
-var a = 1;
+var curState;
+
+const panes = [   'get_pane'
+              ,   'post_pane'
+              ,   'put_pane'
+              ,   'delete_pane'
+              ]
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function setState(state) {
+
+    if (panes.indexOf(state) < 0) {
+        return;
+    }
+
+    const selectedColor = getComputedStyle(document.querySelector('.inputform')).backgroundColor;
+    const normalColor = getComputedStyle(document.querySelector('.menu ul')).backgroundColor;
+
+    $(state).style.backgroundColor = selectedColor;
+
+    panes.forEach( element => {
+        if (element === state) {
+            return;
+        }
+
+        $(element).style.backgroundColor = normalColor;
+    });
+}
+
+function initializeState() {
+    panes.forEach( pane => {
+        $(pane).addEventListener('click', _ => setState(pane), false);
+    });
+
+    setState('get_pane');
+}
+
+window.addEventListener('load', initializeState, false);
